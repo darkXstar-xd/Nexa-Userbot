023275c8ac8f77d71a35f5abbaf4a4876c6ec005
darkXstar-xd <93180058+darkXstar-xd@users.noreply.github.com>
2021-10-28 17:07:38 +0530

    Create __init__.py

---
+++ nexa_userbot/__init__.py
@@ -0,0 +1,19 @@
+import sys
+import time
+import time
+
+from pyrogram import Client
+from config import Config
+
+# Helps
+HELP = {}
+CMD_HELP = {}
+NEXAUB_VERSION = "v0.0.3"
+
+StartTime = time.time()
+
+NEXAUB = Client(
+    api_hash=Config.API_HASH,
+    api_id=Config.APP_ID,
+    session_name=Config.PYRO_STR_SESSION
+)
